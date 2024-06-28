cb2009b39115d3a3dadeea1f988113f63e7feed9
owino-ob <owino.ob@icloud.com>
2024-06-28 12:06:32 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multipy two integers
+ * *@a: first integer
+ * @b: second integer
+ * Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _isupper(int c);
 int _isupper(int c);
+int mul(int a, int b);
 
 #endif
